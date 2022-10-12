6da1a04986acf7d23cd9bc77e2b0c48fc1a14d80
Amresh Prasad <amresh1098@gmail.com>
2022-10-12 18:14:51 +0530

    feat:form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { CompanyPipePipe } from './shared/pipe/company-pipe.pipe';
 import { CoreModule } from './core/core.module';
 
@@ -12,12 +12,10 @@ import { CoreModule } from './core/core.module';
   declarations: [
     AppComponent,
     CompanyPipePipe,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     CoreModule
   ],
   providers: [],
--- src/app/company/company/company-form/company-form.component.ts
+++ src/app/company/company/company-form/company-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-company-form',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-form.component.scss']
 })
 export class CompanyFormComponent implements OnInit {
+  public companyform: FormGroup;
+  public issubmited = false
+  constructor(
+    private fb: FormBuilder,
+  ) {
 
-  constructor() { }
-
-  ngOnInit(): void {
+    this.companyform = this.fb.group({
+      companyname: ['', [Validators.required]],
+      companydescription: ['', [Validators.required]],
+      selecttag: ['', [Validators.required]],
+    })
   }
 
+
+  get validator(): { [key: string]: AbstractControl<any> } {
+    return this.companyform.controls;
+
+  }
+  ngOnInit(): void {
+  }
+  //add/edit Company  function
+  public saveCompany() {
+    this.issubmited = true;
+
+  }
+
+  public reset() {
+    this.companyform.reset()
+    this.issubmited = true;
+  }
 }
