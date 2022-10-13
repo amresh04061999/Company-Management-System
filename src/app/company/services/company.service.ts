5ee222024b4580803981e1a63badc2b9917745c7
Amresh Prasad <amresh1098@gmail.com>
2022-10-13 12:47:17 +0530

    feat:add company in json server

--- src/app/company/services/company.service.ts
+++ src/app/company/services/company.service.ts
@@ -1,7 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { company } from '../model/company.model';
 
 @Injectable()
 export class CompanyService {
 
-  constructor() { }
+  public baseURL: string;
+  constructor(private _http: HttpClient) {
+    this.baseURL = 'http://localhost:3000/'
+  }
+
+  // add company in jsonserver
+  addcomapny(company: company): Observable<company> {
+    // const URL: string = `${this.baseURL}company`;
+    const URL: string = this.baseURL + 'company';
+    return this._http.post<company>(URL, company)
+  }
 }
