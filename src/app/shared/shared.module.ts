673149a9c47fd4363154ecf10a47e83a55accb65
amresh <amresh1098@gmail.com>
2022-10-14 01:30:14 +0530

    feat:firstlaterpipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterPipe } from './pipe/filter.pipe';
+import { FcharactermargePipe } from './pipe/fcharactermarge.pipe';
 
 @NgModule({
-  declarations: [FilterPipe],
+  declarations: [FilterPipe, FcharactermargePipe],
   imports: [CommonModule],
-  exports: [FilterPipe],
+  exports: [FilterPipe, FcharactermargePipe],
 })
 export class SharedModule {}
